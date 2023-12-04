772f91b05f8ba79aea2eada5174baccd7f59ea52
derek <derek@imimr.net>
2023-12-04 12:29:01 +0800

    .

--- src/useRenderPDF.js
+++ src/useRenderPDF.js
@@ -1,20 +1,20 @@
 import { useEffect } from 'react';
 import { useAsync } from 'react-use';
 
-import { proxy, wrap } from 'comlink';
+// import { proxy, wrap } from 'comlink';
+import { wrap } from 'comlink';
 import Worker from './workers/pdf.worker?worker';
 
 export const pdfWorker = wrap(new Worker());
-pdfWorker.onProgress(proxy((info) => console.log(info)));
+// pdfWorker.onProgress(proxy((info) => console.log(info)));
 
-export const useRenderPDF = ({ text }) => {
-  const {
-    value: url,
-    loading,
-    error,
-  } = useAsync(async () => {
-    return pdfWorker.renderPDFInWorker({ text });
-  }, [text]);
+// export const useRenderPDF = ({ text }) => {
+//   const { value: url, loading, error } = useAsync(async () => {
+//     return pdfWorker.renderPDFInWorker({ text });
+//   }, [text]);
+
+export const useRenderPDF = () => {
+  const { value: url, loading, error } = useAsync(async() => pdfWorker.renderPDFInWorker());
 
   useEffect(() => (url ? () => URL.revokeObjectURL(url) : undefined), [url]);
   return { url, loading, error };
